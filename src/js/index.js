fe3828a7ed1c27468eb197c1e4994555b3ab0a82
Rafael-Silveira-Git <rafaeljh99@gmail.com>
2024-02-05 22:05:00 -0300

    fiz alteracoes no reponsivo

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const botao = document.querySelector(".btn-plataformas");
+
+const elementoPlataformas = document.querySelector(
+  ".btn-plataformas .plataformas"
+);
+
+botao.addEventListener("click", () => {
+  elementoPlataformas.classList.toggle("ativo");
+});
